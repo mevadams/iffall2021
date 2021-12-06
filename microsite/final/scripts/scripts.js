cca2f7107be49b4b638209702aed4621f849037c
mevadams <54547643+mevadams@users.noreply.github.com>
2021-12-06 12:00:35 -0600

    Update scripts.js

--- microsite/final/scripts/scripts.js
+++ microsite/final/scripts/scripts.js
@@ -1,18 +1,3 @@
-
-$( document ).ready(function() {			/* Here we're using JQuery to check that the DOM (the document) is ready */
-                
-    $(".dropdown").mouseover(function(){	/* This line says to listen for a "mouseover" event on something with with the class of "dropdown" */
-        $(".dropdown").addClass("open");	/* A mouseover happened, so now we add the class "open" to the element with teh class of "dropdown" */
-                    });
-        
-    $(".dropdown").mouseout(function(){		/* This line says to listen for a "mouseout" event on something with with the class of "dropdown" */
-        $(".dropdown").removeClass("open");	/* A mouseout happened, so now we remove the class "open" from element with the class of "dropdown" */
-        });
-    $(".selected").click(function(){		/* This line says to listen for a "click" event on something with with the class of "selected" */
-        $(".dropdown").removeClass("open");	/* A click happened, so now we remove the class "open" from the element with the class of "dropdown" */
-                    });
-        
-});
 $( document ).ready(function() {			/* Here we're using JQuery to check that the DOM (the document) is ready */
 
 /* The .mouseover and .mouseout events below are what we'll be using on desktop computers where users have a mouse .*/
