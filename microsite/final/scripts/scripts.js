c498f9fff664a5a03d15c8db512b2999e9c06537
mevadams <54547643+mevadams@users.noreply.github.com>
2021-12-06 19:24:47 -0600

    Update scripts.js

--- microsite/final/scripts/scripts.js
+++ microsite/final/scripts/scripts.js
@@ -60,7 +60,7 @@ $( document ).ready(function() {			/* Here we're using JQuery to check that the 
 
     $(".selected").click(function(){		/* This line says to listen for a "click" event on something with with the class of "selected" */
         //console.log("Clicked a link");		/* Writing a log entry to the console, so we can see what events are firing and in what order. */
-        alert("Clicked a link");
+        //alert("Clicked a link");
         $(".dropdown").removeClass("open");	/* A click happened, so now we remove the class "open" from the element with the class of "dropdown" */
         event.stopPropagation();			/* Prevent this event from propagating up through other elements, e.g. prevent it from passing through these buttons to the parent elements. */
     });
